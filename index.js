80c775096519a16a075287506bff40d088ff719e
Donald Coffin <donald.coffin@gmail.com>
2018-08-02 03:36:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  kittens.push(name)
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
   return kittens
 }
